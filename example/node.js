d6d57ef35c269a62af9e856017580336113f8a97
danigb <danigb@gmail.com>
2016-09-13 22:00:58 +0200

    It seems to work with audiojs

--- example/node.js
+++ example/node.js
@@ -1,8 +1,34 @@
 var play = require('audio-play')
 var load = require('../node')
+global.Promise = require('bluebird')
+Promise.longStackTraces()
+
+load.skip = () => Promise.reject('skip')
 
 function err () { console.log('Error:', arguments) }
 
-load(__dirname + '/samples/maeclave.wav').then(play).catch(err)
+console.log('Load a single sample from the filesystem')
+load.skip(__dirname + '/samples/maeclave.wav').then(play).catch(err)
 
-load('https://danigb.github.io/sampled/CR-78/samples/cowbell.wav').then(play).catch(err)
+console.log('Load a single sample from http')
+load.skip('https://danigb.github.io/sampled/CR-78/samples/cowbell.wav').then(play).catch(err)
+
+console.log('Load a midijs soundfont format')
+load('http://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/marimba-mp3.js').then(function (inst) {
+  playSamples(inst, ['C4', 'D4', 'E4', 'F4', 'G4'])
+})
+
+console.log('Load a json with multiple encoded samples')
+load.skip('https://danigb.github.io/sampled/CR-78/CR-78.json').then(function (inst) {
+  var names = Object.keys(inst.samples)
+  playSamples(inst.samples, names)
+}).catch(err)
+
+function playSamples (source, names) {
+  names.forEach(function (name, i) {
+    function playSample () {
+      play(source[name])
+    }
+    setTimeout(playSample, 1000 * i)
+  })
+}
--- node.js
+++ node.js
@@ -3,13 +3,19 @@ var decodeAudio = require('audio-decode')
 var request = require('request')
 var loader = require('.')
 
+// A fix to audio-decode module
+function decode (buffer) {
+  if (buffer instanceof ArrayBuffer) buffer = Buffer(buffer)
+  return decodeAudio(buffer)
+}
+
 module.exports = function load (path, options) {
-  var opts = Object.assign({ decode: decodeAudio }, options)
+  var opts = Object.assign({ decode: decode }, options)
   return loader(null, path, opts)
 }
 
 loader.fetch = function (url, type) {
-  return (isPath(url) ? readFile(url) : sendRequest(url, type)).then(decodeAudio)
+  return isPath(url) ? readFile(url) : sendRequest(url, type)
 }
 
 function isPath (url) { return /^[\\\/\.]/.test(url) }
@@ -17,13 +23,16 @@ function isPath (url) { return /^[\\\/\.]/.test(url) }
 function readFile (url) {
   return new Promise(function (done, reject) {
     fs.readFile(url, function (err, data) {
+      console.log('JODER"', data.toString().slice(0, 10))
       err ? reject(err) : done(data)
     })
   })
 }
+
 function sendRequest (url, type) {
+  var encoding = type === 'arraybuffer' ? null : 'utf8'
   return new Promise(function (done, reject) {
-    request({ url: url, responseType: 'arraybuffer' }, function (err, resp, body) {
+    request({ url: url, encoding: encoding }, function (err, resp, body) {
       if (err) reject(err)
       else if (resp.statusCode !== 200) reject('Status code: ' + resp.statusCode)
       else done(body)
