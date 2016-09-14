7221386ccbf4999968dd314a7a4469d73d814f94
danigb <danigb@gmail.com>
2016-09-14 21:27:39 +0200

    Remove Buffer dependency. loader@node test loads .wav files, but still fails on .mp3

--- test/node-test.js
+++ test/node-test.js
@@ -1,16 +1,17 @@
 /* global describe it */
 var assert = require('assert')
+var isBuffer = require('is-audio-buffer')
 var load = require('..')
 
 function testBuffer (buffer) {
   assert(buffer, 'buffer is present')
-  assert(buffer instanceof Buffer, 'buffer is a buffer')
+  assert(isBuffer(buffer), 'buffer is a buffer')
   return buffer
 }
 
 describe('audio-loader@node', function () {
-  it.skip('load wav files', function () {
-    return load('./example/samples/blip.wav').then(testBuffer)
+  it('load wav files', function () {
+    return load('./example/samples/maeclave.wav').then(testBuffer)
   })
   it.skip('load mp3 files', function () {
     return load('./example/samples/drumroll.mp3').then(testBuffer)
